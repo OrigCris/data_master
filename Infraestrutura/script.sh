e05e9922c2dbe8c949229fb19d987e29571fcc9d
Cristiano Alves <cristianosjunior2012@hotmail.com>
2025-01-31 20:28:53 -0300

    colocando o arquivo script atualizado e a function

--- Infraestrutura/script.sh
+++ Infraestrutura/script.sh
@@ -5,6 +5,7 @@ SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 RESOURCE_GROUP="rsgcjprd001"
 LOCATION="brazilsouth"
 STORAGE_ACCOUNT="stacjprd001"
+EVENTHUB_NAMESPACE_FULLY="evhnscjprd001.servicebus.windows.net"
 EVENTHUB_NAMESPACE="evhnscjprd001"
 EVENTHUB_NAME="evh_neg_prod"
 FUNCTION_APP="funccjprd001"
@@ -21,7 +22,7 @@ az storage account create --name $STORAGE_ACCOUNT --resource-group $RESOURCE_GRO
 
 # Criação do Namespace e Event Hub com o SKU Basic
 az eventhubs namespace create --resource-group $RESOURCE_GROUP --name $EVENTHUB_NAMESPACE --location $LOCATION --sku Basic
-az eventhubs eventhub create --resource-group $RESOURCE_GROUP --namespace-name $EVENTHUB_NAMESPACE --name $EVENTHUB_NAME --cleanup-policy Delete --retention-time-in-hours 1
+az eventhubs eventhub create --resource-group $RESOURCE_GROUP --namespace-name $EVENTHUB_NAMESPACE --name $EVENTHUB_NAME --cleanup-policy Delete --retention-time-in-hours 1 --partition-count 1
 
 # Criação do Plano de Serviço de Aplicativo
 az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --sku B1 --is-linux
@@ -56,23 +57,15 @@ az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalAppId" --
 az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalSecret" --value $SP_SECRET
 az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalTenantId" --value $TENANT_ID
 
-# Criar uma Regra de Autorização para Enviar Mensagens no Event Hub
-az eventhubs namespace authorization-rule create --resource-group $RESOURCE_GROUP --namespace-name $EVENTHUB_NAMESPACE --name PermissaoEnviar --rights Send
-
-# Obter a Connection String da Regra de Autorização (não armazenar no Key Vault)
-EVENTHUB_CONN_STRING=$(az eventhubs namespace authorization-rule keys list --resource-group $RESOURCE_GROUP --namespace-name $EVENTHUB_NAMESPACE --name PermissaoEnviar --query primaryConnectionString -o tsv)
-
 # Atribuir Permissões ao Service Principal para Acessar o Key Vault
 az role assignment create \
     --role "Key Vault Secrets User" \
     --assignee $SP_APP_ID \
     --scope $(az keyvault show --name $KEY_VAULT --query id -o tsv)
 
-# Configurar Variáveis de Ambiente na Function App para Usar a SPN e a Connection String
+# Configurar Variáveis de Ambiente na Function App (não para usar diretamente)
 az functionapp config appsettings set --name $FUNCTION_APP --resource-group $RESOURCE_GROUP --settings \
-    AZURE_CLIENT_ID=$SP_APP_ID \
-    AZURE_CLIENT_SECRET=$SP_SECRET \
-    AZURE_TENANT_ID=$TENANT_ID \
-    EVENTHUB_CONN_STRING=$EVENTHUB_CONN_STRING
+    EVENTHUB_NAMESPACE_FULLY=$EVENTHUB_NAMESPACE_FULLY \
+    EVENTHUB_NAME=$EVENTHUB_NAME
 
 echo "Todos os recursos foram criados e configurados com sucesso!"
