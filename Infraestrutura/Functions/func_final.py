f6281098692064f8d2e6083cbd44669fd610b893
Cristiano Alves <cristianosjunior2012@hotmail.com>
2025-02-08 11:43:55 -0300

    atualizando script

---
+++ Infraestrutura/Functions/func_final.py
@@ -0,0 +1,135 @@
+import os
+import requests
+import json
+import logging
+import azure.functions as func
+from azure.identity import DefaultAzureCredential, ClientSecretCredential
+from azure.keyvault.secrets import SecretClient
+from azure.eventhub import EventHubProducerClient, EventData
+from azure.schemaregistry import SchemaRegistryClient
+from azure.schemaregistry.serializer.avroserializer import AvroSerializer
+
+app = func.FunctionApp()
+
+def get_data_from_api():
+    # API de usuários fictícios
+    api_users = 'https://random-data-api.com/api/v2/users'
+    response = requests.get(url=api_users)
+
+    if response.status_code != 200:
+        logging.error(f"Erro ao buscar dados da API: {response.status_code}")
+        return None
+
+    try:
+        data = response.json()
+    except json.JSONDecodeError:
+        logging.error("Erro ao decodificar JSON da API")
+        return None
+    
+    return data
+
+def connect_eventhub(eventhub_namespace, eventhub_name, spn_credential):
+    # Criar cliente do Event Hub
+    producer_client = EventHubProducerClient(
+        fully_qualified_namespace=eventhub_namespace,
+        eventhub_name=eventhub_name,
+        credential=spn_credential
+    )
+
+    return producer_client
+
+def send_raw_data(data_from_api, eventhub_namespace, eventhub_name, spn_credential):
+    producer_client = connect_eventhub(eventhub_namespace, eventhub_name, spn_credential)
+    event_data_batch = producer_client.create_batch()
+    
+    # Verifique se os dados da API não estão vazios antes de enviá-los
+    if data_from_api:
+        event_data_batch.add(EventData(json.dumps(data_from_api)))
+        producer_client.send_batch(event_data_batch)
+    
+    producer_client.close()
+
+def send_serializable_data(data_from_api, eventhub_namespace, eventhub_name, spn_credential, schemaregistry_fqdn, schema_group):
+    producer_client = connect_eventhub(eventhub_namespace, eventhub_name, spn_credential)
+    event_data_batch = producer_client.create_batch()
+    
+    schema_registry_client = SchemaRegistryClient(
+        fully_qualified_namespace=schemaregistry_fqdn,
+        credential=spn_credential
+    )
+
+    avro_serializer = AvroSerializer(
+        client=schema_registry_client,
+        group_name=schema_group
+    )
+
+    # Obter o esquema Avro registrado no Schema Registry
+    try:
+        schema_definition = schema_registry_client.get_schema(schema_id='e5bb5b1830834f12b14b8a2d4f1e3d9c').definition
+        logging.info(schema_definition)
+    except Exception as e:
+        logging.error(f"Erro ao recuperar o esquema Avro do Schema Registry: {e}")
+        return
+
+    # Criar um batch de eventos
+    event_data_batch = producer_client.create_batch()
+
+    try:
+        serialized_data = avro_serializer.serialize(data_from_api, schema=schema_definition)
+        event_data_batch.add(EventData(serialized_data))
+    except Exception as e:
+        logging.error(f"Erro ao serializar os dados: {e}")
+
+    # Enviar os dados para o Event Hub
+    try:
+        producer_client.send_batch(event_data_batch)
+        logging.info("Mensagem enviada para o Event Hub com Avro Schema!")
+    except Exception as e:
+        logging.error(f"Erro ao enviar dados para o Event Hub: {e}")
+    
+    # Fechar conexões
+    producer_client.close()
+    schema_registry_client.close()
+
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
+def func_negocios(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    # URL do seu Key Vault
+    KEY_VAULT_URL = "https://akvcjprd001.vault.azure.net"
+
+    # Criar a credencial com o managed identity do Function App
+    credential = DefaultAzureCredential()
+
+    # Criar cliente para o Key Vault com as credenciais
+    client = SecretClient(vault_url=KEY_VAULT_URL, credential=credential)
+
+    # Recuperar segredos do Key Vault (Client ID, Client Secret e Tenant ID)
+    try:
+        client_id = client.get_secret("ServicePrincipalAppId").value
+        client_secret = client.get_secret("ServicePrincipalSecret").value
+        tenant_id = client.get_secret("ServicePrincipalTenantId").value
+    except Exception as e:
+        logging.error(f"Erro ao recuperar segredos do Key Vault: {e}")
+        return
+
+    # Criar credencial do SPN
+    spn_credential = ClientSecretCredential(tenant_id, client_id, client_secret)
+
+    # Obter valores das variáveis de ambiente
+    eventhub_namespace = os.getenv('EVENTHUB_NAMESPACE_FULLY')
+    eventhub_name_user = os.getenv('EVENTHUB_NAME_USER')
+    eventhub_name_user_schema = os.getenv('EVENTHUB_NAME_USER_SCHEMA')
+    schemaregistry_fqdn = os.getenv('SCHEMAREGISTRY_FQDN')
+    schema_group = os.getenv('SCHEMA_GROUP')
+    schema_name = os.getenv('SCHEMA_NAME')
+    
+    if not eventhub_namespace or not eventhub_name_user or not eventhub_name_user_schema or not schemaregistry_fqdn or not schema_group or not schema_name:
+        logging.error('Variáveis de ambiente do Event Hub ou Schema Registry não configuradas corretamente.')
+        return    
+
+    data_from_api = get_data_from_api()
+
+    send_raw_data(data_from_api, eventhub_namespace, eventhub_name_user, spn_credential)
+    send_serializable_data(data_from_api, eventhub_namespace, eventhub_name_user_schema, spn_credential, schemaregistry_fqdn, schema_group)
--- Infraestrutura/script.sh
+++ Infraestrutura/script.sh
@@ -5,6 +5,7 @@ SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 RESOURCE_GROUP="rsgcjprd001"
 LOCATION="brazilsouth"
 STORAGE_ACCOUNT="stacjprd001"
+CONTAINER="cont-dt-mst"
 EVENTHUB_NAMESPACE_FULLY="evhnscjprd001.servicebus.windows.net"
 EVENTHUB_NAMESPACE="evhnscjprd001"
 EVENTHUB_NAME_USER="evh_user_random"
@@ -12,17 +13,29 @@ EVENTHUB_NAME_USER_SCHEMA="evh_user_random_schema"
 FUNCTION_APP="funccjprd001"
 PLAN_NAME="aspcjprd001"
 KEY_VAULT="akvcjprd001"
-SERVICE_PRINCIPAL_NAME="spn_func_send"
+SPN_PRODUCER="spn_func_send"
+SPN_CONSUMER="spn_dtb_consumer"
 SCHEMAREGISTRY_FQDN="evhnscjprd001.servicebus.windows.net"
 SCHEMA_GROUP="SchemaFunctions"
 SCHEMA_NAME="UserRandom"
 ACCOUNT_OBJECT_ID=$(az ad signed-in-user show --query id -o tsv)
+DATABRICKS_WORKSPACE="dbwcjprd001"
+DATABRICKS_PLAN="Premium"
 
-# Criação do Resource Group
+############################################# GERANDO TODOS OS RECUROS #####################################################
+
+# Resource Group
 az group create --name $RESOURCE_GROUP --location $LOCATION
+az group create --name $RESOURCE_GROUP_DTB --location $LOCATION
 
-# Criação do Storage Account com suporte a ADLS Gen2
+# Storage Account com suporte a ADLS Gen2
 az storage account create --name $STORAGE_ACCOUNT --resource-group $RESOURCE_GROUP --location $LOCATION --sku Standard_LRS --kind StorageV2 --hns true
+
+# Container e Pastas
+az storage container create --name $CONTAINER --account-name $STORAGE_ACCOUNT --auth-mode login
+az storage fs directory create --account-name $STORAGE_ACCOUNT --file-system $CONTAINER --name bronze
+az storage fs directory create --account-name $STORAGE_ACCOUNT --file-system $CONTAINER --name silver
+az storage fs directory create --account-name $STORAGE_ACCOUNT --file-system $CONTAINER --name gold
 
 # Criação do Namespace e Event Hub com o SKU Basic
 az eventhubs namespace create --resource-group $RESOURCE_GROUP --name $EVENTHUB_NAMESPACE --location $LOCATION --sku Standard
@@ -32,7 +45,7 @@ az eventhubs eventhub create --resource-group $RESOURCE_GROUP --namespace-name $
 # Criação do Plano de Serviço de Aplicativo
 az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --sku B1 --is-linux
 
-# Criação do Function App
+# Criação do Function App e habilitação da Managed Identity
 az functionapp create \
     --resource-group $RESOURCE_GROUP \
     --name $FUNCTION_APP \
@@ -42,31 +55,69 @@ az functionapp create \
     --runtime-version "3.11" \
     --os-type "Linux" \
     --functions-version 4 \
+    --assign-identity \
     --disable-app-insights
 
 # Criação do Key Vault
 az keyvault create --name $KEY_VAULT --resource-group $RESOURCE_GROUP --location $LOCATION
 
-# Criação do Service Principal e armazenando a senha no Key Vault
-SP_DETAILS=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --role Contributor --scopes /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP)
-SP_APP_ID=$(echo $SP_DETAILS | jq -r '.appId')
-SP_SECRET=$(echo $SP_DETAILS | jq -r '.password')
-TENANT_ID=$(echo $SP_DETAILS | jq -r '.tenant')
+# Databricks Workspace
+az config set extension.dynamic_install_allow_preview=true
+az databricks workspace create --name $DATABRICKS_WORKSPACE \
+    --resource-group $RESOURCE_GROUP \
+    --location $LOCATION \
+    --sku $DATABRICKS_PLAN
+
+##################################################### MANAGED IDENTITY ###########################################################
+
+# Function App
+FUNC_MANAGED_ID=$(az functionapp identity show --resource-group $RESOURCE_GROUP --name $FUNCTION_APP --query principalId -o tsv)
+
+#####################################################    PERMISSOES    ###########################################################
 
 az role assignment create \
     --role "Key Vault Secrets Officer" \
     --assignee-object-id $ACCOUNT_OBJECT_ID \
     --scope $(az keyvault show --name $KEY_VAULT --query id -o tsv)
 
+# Criação do Service Principal e armazenando a senha no Key Vault
+SP_DETAILS=$(az ad sp create-for-rbac --name $SPN_PRODUCER --role Contributor --scopes /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP)
+SP_APP_ID=$(echo $SP_DETAILS | jq -r '.appId')
+SP_SECRET=$(echo $SP_DETAILS | jq -r '.password')
+TENANT_ID=$(echo $SP_DETAILS | jq -r '.tenant')
+
+DTB_SP_DETAILS=$(az ad sp create-for-rbac --name $SPN_CONSUMER --role "Contributor" --scopes /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP)
+DTB_SP_APP_ID=$(echo $DTB_SP_DETAILS | jq -r '.appId')
+DTB_SP_SECRET=$(echo $DTB_SP_DETAILS | jq -r '.password')
+DTB_TENANT_ID=$(echo $DTB_SP_DETAILS | jq -r '.tenant')
+
 az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalAppId" --value $SP_APP_ID
 az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalSecret" --value $SP_SECRET
 az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalTenantId" --value $TENANT_ID
+
+az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalDTBAppId" --value $DTB_SP_APP_ID
+az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalDTBSecret" --value $DTB_SP_SECRET
+az keyvault secret set --vault-name $KEY_VAULT --name "ServicePrincipalDTBTenantId" --value $DTB_TENANT_ID
 
 # Atribuir Permissões ao Service Principal para Acessar o Key Vault
 az role assignment create \
     --role "Key Vault Secrets User" \
     --assignee $SP_APP_ID \
     --scope $(az keyvault show --name $KEY_VAULT --query id -o tsv)
+
+# Dar acesso de envio para a SPN
+az role assignment create \
+  --assignee $SP_APP_ID \
+  --role "Azure Event Hubs Data Sender" \
+  --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.EventHub/namespaces/$EVENTHUB_NAMESPACE"
+ 
+# Dar acesso de leitura ao AKV para o Function App
+az role assignment create \
+    --assignee $FUNC_MANAGED_ID \
+    --role "Key Vault Secrets User" \
+    --scope $(az keyvault show --name $KEY_VAULT --query id -o tsv)
+
+################################################### VARIAVEIS DE AMBIENTE ####################################################
 
 # Configurar Variáveis de Ambiente na Function App (não para usar diretamente)
 az functionapp config appsettings set --name $FUNCTION_APP --resource-group $RESOURCE_GROUP --settings \
@@ -76,5 +127,3 @@ az functionapp config appsettings set --name $FUNCTION_APP --resource-group $RES
     SCHEMAREGISTRY_FQDN=$SCHEMAREGISTRY_FQDN \
     SCHEMA_GROUP=$SCHEMA_GROUP \
     SCHEMA_NAME=$SCHEMA_NAME
-
-echo "Todos os recursos foram criados e configurados com sucesso!"
